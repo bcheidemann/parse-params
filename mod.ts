5131d3ad949487e52375e810b5a837462772cab0
Ben Heidemann <ben@heidemann.co.uk>
2024-06-21 09:01:33 -0400

    feat: add parseParamNodesFromX functions

--- mod.ts
+++ mod.ts
@@ -66,6 +66,32 @@ export function parseParamNamesFromFunction(
   return parseParamNamesFromString(fnStr, options);
 }
 
+/**
+ * Parses the parameter AST nodes for the provided function reference.
+ *
+ * @example
+ * ```ts
+ * import { assertEquals } from "@std/assert";
+ * import { parseParamNodesFromFunction } from "@bcheidemann/parse-params";
+ *
+ * function example(arg0: string) {}
+ *
+ * const params = parseParamNodesFromFunction(example);
+ *
+ * assertEquals(params[0], { type: "Identifier", name: "arg0" });
+ * ```
+ *
+ * @param fn The function for which to parse the parameters
+ * @returns Array of parameter AST nodes
+ */
+export function parseParamNodesFromFunction(
+  // deno-lint-ignore no-explicit-any
+  fn: (...args: any[]) => any,
+): Pattern[] {
+  const fnStr = fn.toString();
+  return parseParamNodesFromString(fnStr);
+}
+
 const ANONYMOUS_SYNC_FUNCTION_REGEX = /^function\s*\(/g;
 const ANONYMOUS_ASYNC_FUNCTION_REGEX = /^async function\s*\(/g;
 const SYNC_METHOD_REGEX = /^[\w$]+\(/g;
@@ -93,39 +119,9 @@ export function parseParamNamesFromString(
   fn: string,
   options?: Options,
 ): string[] {
-  if (ANONYMOUS_SYNC_FUNCTION_REGEX.test(fn)) {
-    fn = fn.replace(ANONYMOUS_SYNC_FUNCTION_REGEX, "function fn(");
-  } else if (ANONYMOUS_ASYNC_FUNCTION_REGEX.test(fn)) {
-    fn = fn.replace(ANONYMOUS_ASYNC_FUNCTION_REGEX, "async function fn(");
-  } else if (SYNC_METHOD_REGEX.test(fn)) {
-    fn = fn.replace(SYNC_METHOD_REGEX, "function fn(");
-  } else if (ASYNC_METHOD_REGEX.test(fn)) {
-    fn = fn.replace(ASYNC_METHOD_REGEX, "async function fn(");
-  }
+  const params = parseParamNodesFromString(fn);
 
-  const program = parse(fn, {
-    ecmaVersion: "latest",
-  });
-
-  assert(program.body.length === 1);
-
-  const statement = program.body[0];
-
-  if (statement.type === "FunctionDeclaration") {
-    return mapParams(statement.params);
-  }
-
-  assert(
-    statement.type === "ExpressionStatement",
-    `Expected FunctionDeclaration or ExpressionStatement but received ${statement.type}`,
-  );
-
-  assert(
-    statement.expression.type === "ArrowFunctionExpression",
-    `Expected ArrowFunctionExpression but received ${statement.expression.type}`,
-  );
-
-  return mapParams(statement.expression.params);
+  return mapParams(params);
 
   function mapParams(params: Pattern[]) {
     return params.map((param) => {
@@ -149,4 +145,60 @@ export function parseParamNamesFromString(
       }
     });
   }
+}
+
+/**
+ * Parses the parameter AST nodes for the provided stringified function.
+ *
+ * @example
+ * ```ts
+ * import { assertEquals } from "@std/assert";
+ * import { parseParamNodesFromString } from "@bcheidemann/parse-params";
+ *
+ * function example(arg0: string) {}
+ *
+ * const params = parseParamNodesFromString(example.toString());
+ *
+ * assertEquals(params[0], { type: "Identifier", name: "arg0" });
+ * ```
+ *
+ * @param fn The stringified function for which to parse the parameters
+ * @returns Array of parameter AST nodes
+ */
+export function parseParamNodesFromString(
+  fn: string,
+): Pattern[] {
+  if (ANONYMOUS_SYNC_FUNCTION_REGEX.test(fn)) {
+    fn = fn.replace(ANONYMOUS_SYNC_FUNCTION_REGEX, "function fn(");
+  } else if (ANONYMOUS_ASYNC_FUNCTION_REGEX.test(fn)) {
+    fn = fn.replace(ANONYMOUS_ASYNC_FUNCTION_REGEX, "async function fn(");
+  } else if (SYNC_METHOD_REGEX.test(fn)) {
+    fn = fn.replace(SYNC_METHOD_REGEX, "function fn(");
+  } else if (ASYNC_METHOD_REGEX.test(fn)) {
+    fn = fn.replace(ASYNC_METHOD_REGEX, "async function fn(");
+  }
+
+  const program = parse(fn, {
+    ecmaVersion: "latest",
+  });
+
+  assert(program.body.length === 1);
+
+  const statement = program.body[0];
+
+  if (statement.type === "FunctionDeclaration") {
+    return statement.params;
+  }
+
+  assert(
+    statement.type === "ExpressionStatement",
+    `Expected FunctionDeclaration or ExpressionStatement but received ${statement.type}`,
+  );
+
+  assert(
+    statement.expression.type === "ArrowFunctionExpression",
+    `Expected ArrowFunctionExpression but received ${statement.expression.type}`,
+  );
+
+  return statement.expression.params;
 }
